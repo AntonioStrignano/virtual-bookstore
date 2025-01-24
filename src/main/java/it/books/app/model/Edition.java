1c5480aae708d3c658d900815cf063da67769eab
Innerose <anto19997@gmail.com>
2025-01-24 12:14:24 +0100

    add: edtion tab

--- src/main/java/it/books/app/model/Edition.java
+++ src/main/java/it/books/app/model/Edition.java
@@ -1,5 +1,55 @@
 package it.books.app.model;
 
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "edition")
 public class Edition {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "edition_name", nullable = false, unique = true, columnDefinition = "varchar(20)")
+    private String name;
+
+    @OneToMany(mappedBy = "edition")
+    private List<Book> books;
+
+    // ----------------------------
+    // ----- GETTERS & SETTERS ----
+    // ----------------------------
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
 
 }
