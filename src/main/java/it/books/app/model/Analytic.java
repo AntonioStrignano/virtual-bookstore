37fce32c5077187ae8bc3c8800d69ec9dfd29c3f
Innerose <anto19997@gmail.com>
2025-06-20 12:47:18 +0200

    fix analytics

--- src/main/java/it/books/app/model/Analytic.java
+++ src/main/java/it/books/app/model/Analytic.java
@@ -12,28 +12,75 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="analytics")
+@Table(name = "analytics")
 public class Analytic {
 
-	//id
+	// id
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Integer id;
-	
-	//timestamp
-	@Column(name="event_timestamp", nullable=false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
-	private LocalDateTime	eventTimestamp;
-	//FKs
-    //books
+
+	// timestamp
+	@Column(name = "event_timestamp", nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+	private LocalDateTime eventTimestamp;
+	// FKs
+	// books
 	@ManyToOne
-	@JoinColumn(name="id_book")
+	@JoinColumn(name = "id_book")
 	private Book book;
-	
-	//customer id
+
+	// customer id
 	@ManyToOne
-	@JoinColumn(name="id_customer")
+	@JoinColumn(name = "id_customer")
 	private Customer customerId;
 
-	//event type (linked list)
+	// event type
+	@ManyToOne
+	@JoinColumn(name = "event_type", nullable = false)
+	private AnalyticType analyticType;
+
+	// ----------------------------
+	// ----- GETTERS & SETTERS ----
+	// ----------------------------
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public LocalDateTime getEventTimestamp() {
+		return eventTimestamp;
+	}
+
+	public void setEventTimestamp(LocalDateTime eventTimestamp) {
+		this.eventTimestamp = eventTimestamp;
+	}
+
+	public Book getBook() {
+		return book;
+	}
+
+	public void setBook(Book book) {
+		this.book = book;
+	}
+
+	public Customer getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(Customer customerId) {
+		this.customerId = customerId;
+	}
+
+	public AnalyticType getAnalyticType() {
+		return analyticType;
+	}
+
+	public void setAnalyticType(AnalyticType analyticType) {
+		this.analyticType = analyticType;
+	}
 }
