b54fceb43ce8e18b4de32ac7d4f06f3e9c54ee32
Innerose <anto19997@gmail.com>
2025-06-20 12:55:13 +0200

    start setting notifications

--- src/main/java/it/books/app/model/Notification.java
+++ src/main/java/it/books/app/model/Notification.java
@@ -1,13 +1,35 @@
 package it.books.app.model;
 
-public class Notification {  
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Notification {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+
+	// message
+	@Column(name = "mnesage", nullable = false, columnDefinition = "tinytext")
+	private String message;
+
+	// read status (bool)
+	@Column(name="read_status", nullable = false, columnDefinition = "bool")
+	private Boolean isRead;
 	
-	//message
-	
-	//notification type
-	
-	// FKs
-	
-	//customer (da integrare)
+	// creation date
+	@Column(name="creation_date", nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+	private LocalDateTime creationDate;
+
+	// FKs (da integrare)
+
+	// customer id
+
+	// notification type
 
 }
