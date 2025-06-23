5486840d5328568e258b0f6900c8ff4cc5976789
Innerose <anto19997@gmail.com>
2025-06-23 10:08:48 +0200

    ADD warehouse location

--- src/main/java/it/books/app/model/WarehouseLocation.java
+++ src/main/java/it/books/app/model/WarehouseLocation.java
@@ -1,7 +1,57 @@
 package it.books.app.model;
 
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "warehouse_locations")
 public class WarehouseLocation {
-	
-	//inventory OtM (mapped by "warehouseLocation")
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+
+	@Column(name = "lcation_name", nullable = false, unique = true, columnDefinition = "tinytext")
+	private String location;
+
+	// inventory OtM (mapped by "warehouseLocation")
+	@OneToMany(mappedBy = "warehouseLocation")
+	private List<Inventory> inventoryList;
+
+	// ----------------------------
+	// ----- GETTERS & SETTERS ----
+	// ----------------------------
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public List<Inventory> getInventoryList() {
+		return inventoryList;
+	}
+
+	public void setInventoryList(List<Inventory> inventoryList) {
+		this.inventoryList = inventoryList;
+	}
 
 }
