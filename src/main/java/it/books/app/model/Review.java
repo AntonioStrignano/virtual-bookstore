8fe16775c955691864c9f02655383c520efcc7a8
Innerose <anto19997@gmail.com>
2025-06-23 09:30:36 +0200

    ADD review

--- src/main/java/it/books/app/model/Review.java
+++ src/main/java/it/books/app/model/Review.java
@@ -1,6 +1,98 @@
 package it.books.app.model;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "reviews")
 public class Review {
 
-    //books
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+
+	// rating
+	@Column(name = "rating", nullable = false, columnDefinition = "tinyint")
+	private int rating;
+
+	// comment
+	@Column(name = "comment", nullable = true, columnDefinition = "mediumtext")
+	private String comment;
+
+	// review date
+	@Column(name = "review_date", nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+	private LocalDateTime reviewDate;
+
+	// FKs
+	// book
+	@ManyToOne
+	@JoinColumn(name = "book_id")
+	private Book bookId;
+
+	// customer
+	@ManyToOne
+	@JoinColumn(name = "customer_id")
+	private Customer customerId;
+
+	// ----------------------------
+	// ----- GETTERS & SETTERS ----
+	// ----------------------------
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public LocalDateTime getReviewDate() {
+		return reviewDate;
+	}
+
+	public void setReviewDate(LocalDateTime reviewDate) {
+		this.reviewDate = reviewDate;
+	}
+
+	public Book getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(Book bookId) {
+		this.bookId = bookId;
+	}
+
+	public Customer getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(Customer customerId) {
+		this.customerId = customerId;
+	}
+
 }
